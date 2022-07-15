8184e4df15a1c2b4815528fe227637aa13b00525
anand <aady03aady03@gmail.com>
2022-07-16 00:10:23 +0530

    initiatorName Changed

--- api/models.py
+++ api/models.py
@@ -24,7 +24,7 @@ class ProductDetail(models.Model):
 class MyChats(models.Model):
     intiatorId  = models.CharField(max_length= 30) 
     friendId = models.CharField(max_length= 30)
-    initiatorName = models.CharField(max_length=200,default='',blank=True)
+    initiatorName = models.CharField(max_length=200,blank=True)
     friendName = models.CharField(max_length= 200)
     lastMessage = models.TextField(blank=True)
     timeStampMessage = models.CharField(max_length= 300)
